71994de9d99b92f58066e337d361c5f81b7f36c1
Jaakko Lukkari <jaakko@applifier.com>
2015-02-23 01:06:39 +0200

    User custom loader in tests

--- test/rjss-spec.js
+++ test/rjss-spec.js
@@ -4,14 +4,41 @@ require('chai').should();
 
 
 var RJSS = require('../lib/rjss');
+var rjss = new RJSS();
+
+require.extensions['.rjss'] = function(module, filename) {
+  var result = rjss.parseFile(filename);
+  return module._compile(result.code, filename);
+};
 
 describe('rjss', function () {
   it('should process simple rjss file with two rules', function () {
-    var rjss = new RJSS();
+    var simple = require('../test_data/simple.rjss');
 
-    var result = rjss.parseFile(require.resolve('../test_data/simple.rjss'));
+    var main = simple('main');
+    main.top.should.equal(10);
+    main.left.should.equal(0);
+    main.width.should.equal('100px');
+    main.height.should.equal(20);
+    main.lineHeight.should.equal(20);
+    main.fontSize.should.equal(12);
 
-    console.log(result);
+    main = simple();
+    main.top.should.equal(10);
+    main.left.should.equal(0);
+    main.width.should.equal('100px');
+    main.height.should.equal(20);
+    main.lineHeight.should.equal(20);
+    main.fontSize.should.equal(12);
+
+    var button = simple('button');
+    button.top.should.equal(10);
+    button.left.should.equal(0);
+    button.width.should.equal('100px');
+    button.height.should.equal(20);
+    button.lineHeight.should.equal(20);
+    button.fontSize.should.equal(12);
+
   });
 
   it('should process rjss file with one rule and variables', function () {
